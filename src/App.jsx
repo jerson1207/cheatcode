44327b8b1badd5e5bdb011a4adb0c359ef239df8
jersonjabat <jersonjabatstudent@gmail.com>
2024-12-03 19:10:03 +0800

    add tailwind and react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import SliderPage from './pages/SliderPage';
 
 function App() {
   return (
-    <div>
-      this is a start of react
-    </div>
-  )
+    <BrowserRouter>
+      <div className="flex h-screen">
+        {/* Sidebar (Left Navigation) */}
+        <aside className="w-64 bg-gray-800 text-white p-6">
+          <h2 className="text-2xl font-bold mb-8">Admin Panel</h2>
+          <nav>
+            <ul>
+              <li className="mb-4">
+                <Link to="/" className="text-white hover:underline">Home</Link>
+              </li>
+              <li className="mb-4">
+                <Link to="/slider" className="text-white hover:underline">Slider</Link>
+              </li>
+            </ul>
+          </nav>
+        </aside>
+
+        {/* Content Area */}
+        <main className="flex-1 bg-gray-100 p-6">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/slider" element={<SliderPage />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/SliderPage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class SliderPage extends Component {
+  render() {
+    return (
+      <div>
+        Slider
+      </div>
+    )
+  }
+}
+
+export default SliderPage
